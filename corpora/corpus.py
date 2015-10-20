6433dd2eac5cd6b1abf2c4e8a02924f1284dc75a
Joris Borgdorff <j.borgdorff@esciencecenter.nl>
2015-10-20 19:47:08 +0200

    Made corpus into a class

--- corpora/corpus.py
+++ corpora/corpus.py
@@ -2,10 +2,78 @@ import gensim
 import pickle
 import pandas as pd
 import nltk
-from pattern.nl import parse, split
+import pattern.nl as nlp
 import sys
 import os
+import progressbar
 from time import time
+
+class Corpus(object):
+    def __init__(self):
+        self.dic = None
+        self.corpus = None
+        self.documents = []
+        self.metadata = []
+
+    def add_file(self, fp, metadata={}):
+        self.add_text(fp.read(), metadata)
+
+    def add_text(self, text, metadata={}):
+        tokens = self.tokenize_english(text)
+        self.documents.append(tokens)
+        self.metadata.append(metadata)
+
+    def tokenize_english(self, text):
+        stopwords = nltk.corpus.stopwords.words('english')
+        stopwords += ['', '.', ',', '?', '(', ')', ',', ':', "'", u'``', u"''",
+                      ';','-','!','%','&','...','=', '>', '#', '_', '~', '+',
+                      '*', '/', '\\', '[', ']', '|']
+        stopwords += [u'\u2019', u'\u2018', u'\u2013', u'\u2022', u'\u2014',
+                      u'\uf02d',u'\u20ac',u'\u2026']
+
+        keepTags = [
+            # None, u'(', u')', u',', u'.', u'<notranslation>damesbladen', # extras
+            # u'CC', # conjunction
+            # u'CD', # cardinal (numbers)
+            # u'DT', # determiner (de, het)
+            u'FW', # foreign word
+            # u'IN', #conjunction
+            u'JJ', # adjectives -- # u'JJR', u'JJS',
+            # u'MD', # Modal verb
+            u'NN', u'NNP', u'NNPS', u'NNS', # Nouns
+            # u'PRP', # Pronouns -- # u'PRP$',
+            u'RB', # adverb
+            u'RP', # adverb
+            # u'SYM', # Symbol
+            # u'TO', # infinitival to
+            # u'UH', # interjection
+            u'VB', u'VBD', u'VBG', u'VBN', u'VBP', u'VBZ' # Verb forms
+        ]
+        return self.tokenize(text, keepTags, exclude_words=stopwords)
+
+    def tokenize(self, text, nlp_tags, exclude_words = []):
+        words = []
+        # slightly faster w in collection testing
+        nlp_tags = frozenset(nlp_tags)
+        exclude_words = frozenset(exclude_words)
+        for s in nlp.split(nlp.parse(text)):
+            for word, tag in s.tagged:
+                if tag in nlp_tags:
+                    word = word.lower()
+                    if word not in exclude_words:
+                        words.append(word)
+
+        return words
+
+    def generate_dictionary(self):
+        self.dic = gensim.corpora.Dictionary(self.documents)
+
+    def generate_bag_of_words(self):
+        if self.dic is None:
+            self.generate_dictionary()
+
+        self.corpus = [self.dic.doc2bow(tokens) for tokens in self.documents]
+
 
 def load_vraagtekst_corpus(documents_filename):
     with open(documents_filename, 'r') as f:
@@ -20,73 +88,41 @@ def load_vraagtekst_corpus(documents_filename):
     return (dic, corpus, data_ppl)
 
 
-def clean_plaintext_corpus(text):
-	stopwords = nltk.corpus.stopwords.words('dutch')
-	stopwords += nltk.corpus.stopwords.words('english')
-	stopwords += ['','.',',','?','(',')',',',':',"'",u'``',u"''",';','-','!','%','&','...','=','we','wij']
-	stopwords += [u'\u2019',u'\u2018',u'\u2013',u'\u2022',u'\u2014',u'\uf02d',u'\u20ac',u'\u2026']
-
-	keepTags = [
-	    # None, u'(', u')', u',', u'.', u'<notranslation>damesbladen', # extras
-	    # u'CC', # conjunction
-	    # u'CD', # cardinal (numbers)
-	    # u'DT', # determiner (de, het)
-	    u'FW', # foreign word
-	    # u'IN', #conjunction
-	    u'JJ', # adjectives -- # u'JJR', u'JJS',
-	    # u'MD', # Modal verb
-	    u'NN', u'NNP', u'NNPS', u'NNS', # Nouns
-	    # u'PRP', # Pronouns -- # u'PRP$',
-	    u'RB', # adverb
-	    u'RP', # adverb
-	    # u'SYM', # Symbol
-	    # u'TO', # infinitival to
-	    # u'UH', # interjection
-	    u'VB', u'VBD', u'VBG', u'VBN', u'VBP', u'VBZ' # Verb forms
-	]
-	return filter_nlp_tags(text, keepTags, exclude_words=stopwords)
-
-
-def filter_nlp_tags(text, nlp_tags, exclude_words = []):
-    words = []
-    for s in split(parse(text)):
-        for word, tag in s.tagged:
-            if tag in nlp_tags:
-                words.append(word.lower())
-    words = [ w for w in words if w not in exclude_words ]
-    return words
-
-
 def load_enron_corpus(directory):
-	total_size = 0
-	for root, dirs, files in os.walk(directory):
-		total_size += len(files)
+    total_size = 0
+    for root, dirs, files in os.walk(directory):
+        total_size += len(files)
 
-	print("reading {0} files".format(total_size))
+    print("reading {0} files".format(total_size))
 
-	start_time = time()
-	documents = []
-	for user in os.listdir(directory):
-		print("User: {0}".format(user))
-		for root, dirs, files in os.walk(os.path.join(directory, user)):
-			mailbox = os.path.basename(root)
-			if len(files) > 0:
-				print("Mailbox: {0} ({1:.0%}, {2:.2f} seconds)".format(mailbox, len(documents)/float(total_size), time() - start_time))
-			for email in files:
-				doc = {'user': user, 'mailbox': mailbox, 'directory': root}
-				with open(os.path.join(root, email)) as f:
-					doc['tokens'] = clean_plaintext_corpus(f.read())
-				documents.append(doc)
+    corpus = Corpus()
+    start_time = time()
+    bar = progressbar.ProgressBar(
+        maxval=total_size,
+        widgets=[progressbar.Percentage(), ' ', progressbar.Bar('=', '[', ']'),
+                 ' ', progressbar.widgets.ETA()])
+    bar.start()
 
-	return documents
+    for user in os.listdir(directory):
+        for root, dirs, files in os.walk(os.path.join(directory, user)):
+            mailbox = os.path.basename(root)
+            for email in files:
+                metadata = {'user': user, 'mailbox': mailbox, 'directory': root}
+                with open(os.path.join(root, email)) as f:
+                    corpus.add_file(f, metadata)
 
+            bar.update(len(corpus.documents))
+
+    bar.finish()
+
+    return corpus
 
 if __name__ == '__main__':
-	if len(sys.argv) != 3:
-		print("Usage: corpus.py enron_directory enron_pickle_file.pkl")
-		sys.exit(1)
+    if len(sys.argv) != 3:
+        print("Usage: corpus.py enron_directory enron_pickle_file.pkl")
+        sys.exit(1)
 
-	docs = load_enron_corpus(sys.argv[1])
-	print("Emails: {0}".format(len(docs)))
-	with open(sys.argv[2], 'wb') as f:
-		pickle.dump(docs, f)
+    corpus = load_enron_corpus(sys.argv[1])
+    print("Emails: {0}".format(len(corpus.documents)))
+    with open(sys.argv[2], 'wb') as f:
+        pickle.dump({'tokens': corpus.documents, 'metadata': corpus.metadata}, f)
