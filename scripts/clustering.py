d330cbfbb0a678d62a6b984d299ec173f19b905a
daniela <remenska@gmail.com>
2015-12-01 12:59:14 +0100

    .

--- scripts/clustering.py
+++ scripts/clustering.py
@@ -5,32 +5,25 @@ from corpora.scikit import ScikitLda
 import zipfile
 import os
 import tarfile
+import gzip
+import zlib
 if __name__ == '__main__':
 
-    dirname = "/home/daniela/git/analyzing-corpora/data/data/enron_out_0.1/"
+    dirname = "./data/data/enron_out_0.1/"
 
-    for filename in os.listdir(dirname):
-        # archive = zipfile.ZipFile(dirname+filename, mode='r')
-        tar = tarfile.open(dirname+filename, "r:bz2")
-        # print(tar.name)
-        for tarinfo in tar:
-            print tarinfo.name, "is", tarinfo.size, "bytes in size and is",
-            if tarinfo.isreg():
-                f = tar.extractfile(tarinfo)
-                print(f.name)
-                lda = ScikitLda.load(dirname+f)
-                # lda = ScikitLda.load(f)
+    for subdir in os.listdir(dirname):
+        # print("subdir", subdir)
+        for file in os.listdir(dirname+subdir):
+            if file.endswith('pkl'):
+                lda = ScikitLda.load(dirname+subdir+"/"+file)
+                topics = []
+                print(lda.topics)
+                for topic in lda.topics:
+                    topics.append(topic / topic.sum())
 
-
-    # lda = ScikitLda.load("/home/daniela/git/analyzing-corpora/data/data/enron_out_0.1/lda_4/lda_4.pkl")
-    #
-    # topics = []
-    # for topic in lda.topics:
-    #     topics.append(topic / topic.sum())
-    #
-    # for i in range(len(topics)):
-    #     for j in range(i + 1, len(topics)):
-    #         print 'Topic#%d Topic#%d %f' % (i, j, cosine(topics[i], topics[j]))
+                for i in range(len(topics)):
+                    for j in range(i + 1, len(topics)):
+                        print 'Topic#%d Topic#%d %f' % (i, j, cosine(topics[i], topics[j]))
 
 
 
