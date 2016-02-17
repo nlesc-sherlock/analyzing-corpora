2255ef96eb579fdc6510971f3a157c068762deef
Vincent van Hees <vincentvanhees@gmail.com>
2016-02-17 14:46:20 +0100

    prepare variables for visualisation in R now wrapped as a function

---
+++ scripts/example_prepare_visualisation.R
@@ -0,0 +1,19 @@
+rm(list=ls())
+setwd("~/sherlock/topic group/github/analyzing-corpora/") #set working directory
+pathM = "scripts/shinyapp/" # directory of shinyapp
+datadir = "data/" #directory of dictionary
+
+source("scripts/prepare_visualisation.R") # directory of shinyapp
+
+# GET WORD DICTIONARY
+dic = read.csv(file=paste0(datadir,"enron_dic.csv"),stringsAsFactors=FALSE,sep ="\t",header=FALSE) 
+names(dic) =c("id","word")
+# GET WORDS X TOPICS
+woto = read.csv(paste0(datadir,"enron_lda_15.csv"),header=FALSE) 
+# GET DOCUMENTS x TOPICS
+doto = read.csv(file=paste0(datadir,"enron_document_topics_15.csv"),sep ="\t",header=FALSE) 
+# GET DOCUMENT DICTIONARY (id -> filename)
+# ... not available yet
+
+
+out = prepare_visualisation(dic,woto,doto)
--- scripts/prepare_visualisation.R
+++ scripts/prepare_visualisation.R
@@ -1,41 +1,29 @@
-rm(list=ls())
-
-setwd("~/sherlock/topic group/github/analyzing-corpora/") #set working directory
-pathM = "scripts/shinyapp/" # directory of shinyapp
-datadir = "data/" #directory of dictionary
-
-# GET WORD DICTIONARY
-dic = read.csv(file=paste0(datadir,"enron_dic.csv"),stringsAsFactors=FALSE,sep ="\t",header=FALSE) 
-names(dic) =c("id","word")
-
-# GET WORDS X TOPICS
-woto = read.csv(paste0(datadir,"enron_lda_15.csv"),header=FALSE) 
-
-# GET DOCUMENTS x TOPICS
-doto = read.csv(file=paste0(datadir,"enron_document_topics_15.csv"),sep ="\t",header=FALSE) 
-
-# GET DOCUMENT DICTIONARY (id -> filename)
-# ... not available yet
-
-# derive summaries for wordxtopic
-RS1 = rowSums(woto)
-wotonorm = as.data.frame(woto / RS1) # normalize probabilities per word
-winningwoto = max.col(wotonorm)     # <--- most popular topic per word
-Nwordsto = table(winningwoto)   # <---  number of (winning) words per topic
-wotoorder = matrix(0,nrow(wotonorm),ncol(wotonorm))
-wotoorder_words = matrix("",nrow(wotonorm),ncol(wotonorm))
-wotonorm$id = 1:nrow(wotonorm)
-for (i in 1:(ncol(wotonorm)-1)) {
-  wotoorder[,i] = wotonorm$id[order(-wotonorm[,i])]   # <--- ranking of wordsindeces by their distance to a topic
-  wotoorder_words[,i] = dic[wotoorder[,i],2]          # <--- ranking of words by their distance to a topic
+prepare_visualisation = function(dic,woto,doto) {
+  # derive summaries for wordxtopic
+  RS1 = rowSums(woto)
+  wotonorm = as.data.frame(woto / RS1) # normalize probabilities per word
+  winningwoto = max.col(wotonorm)     # <--- most popular topic per word
+  Nwordsto = table(winningwoto)   # <--- number of (winning) words per topic
+  wotoorder = matrix(0,nrow(wotonorm),ncol(wotonorm))
+  wotoorder_words = matrix("",nrow(wotonorm),ncol(wotonorm))
+  wotonorm$id = 1:nrow(wotonorm)
+  for (i in 1:(ncol(wotonorm)-1)) {
+    wotoorder[,i] = wotonorm$id[order(-wotonorm[,i])]   # <--- ranking of wordsindeces by their distance to a topic
+    wotoorder_words[,i] = dic[wotoorder[,i],2]          # <--- ranking of words by their distance to a topic
+  }
+  wotoorder = wotoorder[,-ncol(wotoorder)]
+  # derive summaries for documentxtopic
+  RS2 = rowSums(doto)
+  winningtodo = max.col(doto)     # <--- most popular topic per document
+  Ndocsto = table(winningtodo)    # <--- number of (winning) documents per topic
+  dotonorm = as.data.frame(doto / RS2) # normalize probabilities per word
+  dotoorder = matrix(0,nrow(dotonorm),ncol(dotonorm))
+  dotonorm$id = 1:nrow(dotonorm)
+  for (i in 1:(ncol(dotonorm)-1)) {
+    dotoorder[,i] = dotonorm$id[order(-dotonorm[,i])]   # <--- ranking of documents by topics
+  }
+  dotoorder = dotoorder[,-ncol(dotoorder)]
+  invisible(list(winningwoto=winningwoto,wotoorder=wotoorder,wotoorder_words=wotoorder_words,
+                 winningtodo=winningtodo,dotoorder=dotoorder))
 }
-# derive summaries for documentxtopic
-RS2 = rowSums(doto)
-winningtodo = max.col(doto)     # <--- most popular topic per document
-Ndocsto = table(winningtodo)    # <--- number of (winning) documents per topic
-dotonorm = as.data.frame(doto / RS2) # normalize probabilities per word
-dotoorder = matrix(0,nrow(dotonorm),ncol(dotonorm))
-dotonorm$id = 1:nrow(dotonorm)
-for (i in 1:(ncol(dotonorm)-1)) {
-  dotoorder[,i] = dotonorm$id[order(-dotonorm[,i])]   # <--- ranking of documents by topics
-}
+
