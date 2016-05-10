e0c75f8b58fbce3a850073c7462e39f6f98b55b8
Carlos Martinez <c.martinez@esciencecenter.nl>
2016-05-10 16:17:25 +0200

    Roland flask

--- server.py
+++ server.py
@@ -2,19 +2,45 @@ from flask import Flask, jsonify
 from flask_restful import reqparse
 from flask.ext.cors import CORS
 
+import numpy as np
+
 app = Flask(__name__)
 CORS(app)
 
+# Topic x words matrix
+_data = np.loadtxt('enron_small_lda_transposed.csv', delimiter=',')
+
+_idToWord = {}
+with open('enron_small_dic.csv', 'r') as fin:
+    for line in fin:
+        line = line.strip()
+        cols = line.split('\t')
+        num = int(cols[0])
+        word = cols[1]
+        _idToWord[num] = word
+
+def getWordsInTopic(n):
+    pct = 0.20
+
+    wordIds = _data[:,n]
+    wordIds /= wordIds.sum()
+    sortIdx = wordIds.argsort()
+    sortIdx = sortIdx[::-1]
+    topWordIds = sortIdx[wordIds[sortIdx].cumsum()<pct]
+
+    wordArray = [ {"name": _idToWord[wId], "size": 1} for wId in topWordIds ]
+    return wordArray
+
 def getTopic(n):
     topic = {
-        'name'    : 'nameOfTopic',
-        'children': 'xxx'
+        'name'    : 'Topic %d'%n,
+        'children': getWordsInTopic(n)
     }
     return topic
 
 @app.route('/ronald')
 def ronaldData():
-    topics = [ getTopic(n) for n in range(10) ]
+    topics = [ getTopic(n) for n in range(_data.shape[1]) ]
     data = {
         'name'    : 'ENRON',
         'children': topics
